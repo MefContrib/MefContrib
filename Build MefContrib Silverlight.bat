eb32478b4c8b4811c246f38a56ad29999c0b1853
Andreas Hakansson <andreas@selfinflicted.org>
2010-05-23 22:17:37 +0200

    Added builder helper files to make building easier

---
+++ Build MefContrib Silverlight.bat
@@ -0,0 +1,3 @@
+powershell -Command "& {Import-Module .\build\psake.psm1; Invoke-psake .\build\build_silverlight.ps1 -framework 4.0x86}"
+
+Pause
---
+++ Build MefContrib.bat
@@ -0,0 +1,3 @@
+powershell -Command "& {Import-Module .\build\psake.psm1; Invoke-psake .\build\build.ps1 -framework 4.0}"
+
+Pause
