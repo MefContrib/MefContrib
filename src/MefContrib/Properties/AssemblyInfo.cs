c94bd8bde39cd2ab5127aae1951d52c405af25cd
Andreas Hakansson <andreas@selfinflicted.org>
2010-05-23 21:56:11 +0200

    Removed referenced to SharedAssemblyInfo in Silverlight projects and added reference to AssemblyInfo in MefContrib and MefContrib.Silverlight

---
+++ src/MefContrib/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliantAttribute(false)]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyTitleAttribute("MefContrib 1.0.0.0")]
+[assembly: AssemblyDescriptionAttribute("Community-developed library of extensions to the Managed Extensibility Framework (MEF).")]
+[assembly: AssemblyCompanyAttribute("MefContrib")]
+[assembly: AssemblyProductAttribute("MefContrib 1.0.0.0")]
+[assembly: AssemblyCopyrightAttribute("Copyright © MefContrib 2009 - 2010")]
+[assembly: AssemblyVersionAttribute("1.0.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: AssemblyDelaySignAttribute(false)]
+
