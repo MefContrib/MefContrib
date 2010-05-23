f339afdcf4ba51b02c16f06e06598c8466041007
Andreas Hakansson <andreas@selfinflicted.org>
2010-05-23 21:22:20 +0200

    Removed invalid file references

--- src/MefContrib/Properties/SharedAssemblyInfo.cs
+++ src/MefContrib/Properties/SharedAssemblyInfo.cs
@@ -1,12 +1,17 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
-[assembly: AssemblyCompany("MEF Contrib")]
-[assembly: AssemblyConfiguration("release")]
-[assembly: AssemblyCopyright("Copyright © MEF Contrib 2009")]
-[assembly: AssemblyDescription("Community-developed library of extensions to the Managed Extensibility Framework (MEF).")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyProduct("MefContrib")]
-[assembly: AssemblyTitle("MefContrib")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CLSCompliantAttribute(false )]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyTitleAttribute("MefContrib 1.0.0.0")]
+[assembly: AssemblyDescriptionAttribute("Community-developed library of extensions to the Managed Extensibility Framework (MEF).")]
+[assembly: AssemblyCompanyAttribute("MefContrib")]
+[assembly: AssemblyProductAttribute("MefContrib 1.0.0.0")]
+[assembly: AssemblyCopyrightAttribute("Copyright © MefContrib 2009")]
+[assembly: AssemblyVersionAttribute("1.0.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0.0 / 30ab17b")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: AssemblyDelaySignAttribute(false)]
+
