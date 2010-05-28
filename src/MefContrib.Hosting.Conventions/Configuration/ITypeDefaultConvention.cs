3ee40a311d92a23a339c64b6c3226358c8e52d65
Andreas Hakansson <andreas@selfinflicted.org>
2010-05-28 23:49:23 +0200

    Added ITypeDefaultConvention

---
+++ src/MefContrib.Hosting.Conventions/Configuration/ITypeDefaultConvention.cs
@@ -0,0 +1,25 @@
+namespace MefContrib.Hosting.Conventions.Configuration
+{
+    using System;
+
+    public interface ITypeDefaultConvention
+    {
+        /// <summary>
+        /// Gets or sets the name of the contract.
+        /// </summary>
+        /// <value>A <see cref="string"/> containing the name of the contract.</value>
+        string ContractName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Type"/> of the contract.
+        /// </summary>
+        /// <value>The <see cref="Type"/> of the contract.</value>
+        Type ContractType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="Type"/> that the convention applies to.
+        /// </summary>
+        /// <value>The <see cref="Type"/> that the convention applies to.</value>
+        Type TargetType { get; set; }
+    }
+}
