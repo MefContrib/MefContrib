d8bd4964b9c8c4547fe2c673f004688ec0a7644b
unknown <gblock@.redmond.corp.microsoft.com>
2010-09-26 20:58:46 -0700

    Support for dynamic proxy

---
+++ src/MefContrib.Interception.Castle/DynamicProxyInterceptor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.DynamicProxy;
+using MefContrib.Interception;
+
+namespace Composition.Interception.DynamicProxy
+{
+    public class DynamicProxyInterceptor : IExportedValueInterceptor
+    {
+        private readonly IInterceptor[] _interceptors;
+        private static readonly ProxyGenerator _generator = new ProxyGenerator();
+
+        public DynamicProxyInterceptor(params IInterceptor[] interceptors)
+        {
+            _interceptors = interceptors;
+        }
+
+        public object Intercept(object value)
+        {
+            ProxyGenerationOptions options = new ProxyGenerationOptions();
+
+            var interfaces = value.GetType().GetInterfaces();
+            Type proxyInterface = interfaces.FirstOrDefault();
+            Type[] additionalInterfaces = interfaces.Skip(1).ToArray();
+            var proxy = _generator.CreateInterfaceProxyWithTargetInterface(proxyInterface, additionalInterfaces, value, _interceptors);            
+            return proxy;
+        }
+    }
+}
