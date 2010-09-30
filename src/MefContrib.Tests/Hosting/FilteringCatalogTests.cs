d217dfc596acc853c9b4d7bafafbc71013039ad8
Piotr Wlodek <piotr.wlodek@gmail.com>
2010-09-30 12:09:27 +0200

    Renamed FilteredCatalog to FilteringCatalog

---
+++ src/MefContrib.Tests/Hosting/FilteringCatalogTests.cs
@@ -0,0 +1,203 @@
+using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using MefContrib.Hosting;
+using NUnit.Framework;
+
+namespace MefContrib.Tests.Hosting
+{
+    [TestFixture]
+    public class FilteringCatalogTests
+    {
+        public interface IMetadataComponent {}
+
+        [Export(typeof(IMetadataComponent))]
+        [PartMetadata("key", "value")]
+        public class Component1
+        {
+        }
+
+        [Export(typeof(IMetadataComponent))]
+        public class Component2
+        {
+        }
+
+        [Test]
+        public void FilterBasedOnMetadataUsingContainsMetadataTest()
+        {
+            var catalog = new AssemblyCatalog(typeof(FilteringCatalogTests).Assembly);
+            var filteredCatalog = new FilteringCatalog(catalog, new ContainsMetadata("key", "value"));
+            var container = new CompositionContainer(filteredCatalog);
+            var components = container.GetExports<IMetadataComponent>();
+
+            Assert.That(components, Is.Not.Null);
+            Assert.That(components.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FilterBasedOnMetadataUsingLambdaExpressionTest()
+        {
+            var catalog = new AssemblyCatalog(typeof(FilteringCatalogTests).Assembly);
+            var filteredCatalog = new FilteringCatalog(catalog,
+                                                      partDefinition => partDefinition.Metadata.ContainsKey("key") &&
+                                                                        partDefinition.Metadata["key"].Equals("value"));
+            var container = new CompositionContainer(filteredCatalog);
+            var components = container.GetExports<IMetadataComponent>();
+
+            Assert.That(components, Is.Not.Null);
+            Assert.That(components.Count(), Is.EqualTo(1));
+        }
+
+        public interface ILifetimeComponent
+        {
+        }
+
+        [Export(typeof(ILifetimeComponent))]
+        public class LifetimeComponent1 : ILifetimeComponent
+        {
+        }
+
+        [Export(typeof(ILifetimeComponent))]
+        [PartCreationPolicy(CreationPolicy.Shared)]
+        public class LifetimeComponent2 : ILifetimeComponent
+        {
+        }
+
+        [Export(typeof(ILifetimeComponent))]
+        [PartCreationPolicy(CreationPolicy.NonShared)]
+        public class LifetimeComponent3 : ILifetimeComponent
+        {
+        }
+
+        [Export(typeof(ILifetimeComponent))]
+        [PartCreationPolicy(CreationPolicy.Any)]
+        public class LifetimeComponent4 : ILifetimeComponent
+        {
+        }
+
+        [Test]
+        public void FilterBasedOnSharedLifetimeUsingHasCreationPolicyTest()
+        {
+            var catalog = new AssemblyCatalog(typeof(FilteringCatalogTests).Assembly);
+            var filteredCatalog = new FilteringCatalog(catalog, new HasCreationPolicy(CreationPolicy.Shared));
+            var container = new CompositionContainer(filteredCatalog);
+            var components = container.GetExports<ILifetimeComponent>();
+
+            Assert.That(components, Is.Not.Null);
+            Assert.That(components.Count(), Is.EqualTo(1));
+            Assert.That(components.First().Value.GetType(), Is.EqualTo(typeof(LifetimeComponent2)));
+        }
+
+        [Test]
+        public void FilterBasedOnNonSharedLifetimeUsingHasCreationPolicyTest()
+        {
+            var catalog = new AssemblyCatalog(typeof(FilteringCatalogTests).Assembly);
+            var filteredCatalog = new FilteringCatalog(catalog, new HasCreationPolicy(CreationPolicy.NonShared));
+            var container = new CompositionContainer(filteredCatalog);
+            var components = container.GetExports<ILifetimeComponent>();
+
+            Assert.That(components, Is.Not.Null);
+            Assert.That(components.Count(), Is.EqualTo(1));
+            Assert.That(components.First().Value.GetType(), Is.EqualTo(typeof(LifetimeComponent3)));
+        }
+
+        [Test]
+        public void FilterBasedOnAnyLifetimeUsingHasCreationPolicyTest()
+        {
+            var catalog = new AssemblyCatalog(typeof(FilteringCatalogTests).Assembly);
+            var filteredCatalog = new FilteringCatalog(catalog, new HasCreationPolicy(CreationPolicy.Any));
+            var container = new CompositionContainer(filteredCatalog);
+            var components = container.GetExports<ILifetimeComponent>();
+
+            Assert.That(components, Is.Not.Null);
+            Assert.That(components.Count(), Is.EqualTo(2));
+            Assert.That(components.Select(t => t.Value).OfType<LifetimeComponent1>().First().GetType(), Is.EqualTo(typeof(LifetimeComponent1)));
+            Assert.That(components.Select(t => t.Value).OfType<LifetimeComponent4>().First().GetType(), Is.EqualTo(typeof(LifetimeComponent4)));
+        }
+
+        [Export]
+        [PartCreationPolicy(CreationPolicy.NonShared)]
+        public class Root : IDisposable
+        {
+            [Import]
+            public Dep1 Dep { get; set; }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+
+            public bool Disposed { get; private set; }
+        }
+
+        [Export]
+        [PartCreationPolicy(CreationPolicy.NonShared)]
+        public class Dep1 : IDisposable
+        {
+            [Import]
+            public Dep2 Dep { get; set; }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+
+            public bool Disposed { get; private set; }
+        }
+
+        [Export]
+        [PartCreationPolicy(CreationPolicy.Shared)]
+        public class Dep2 : IDisposable
+        {
+            [Import]
+            public Dep3 Dep { get; set; }
+
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+
+            public bool Disposed { get; private set; }
+        }
+
+        [Export]
+        [PartCreationPolicy(CreationPolicy.NonShared)]
+        public class Dep3 : IDisposable
+        {
+            public void Dispose()
+            {
+                Disposed = true;
+            }
+
+            public bool Disposed { get; private set; }
+        }
+
+        [Test]
+        public void ParentChildContainerTest()
+        {
+            var catalog = new AssemblyCatalog(typeof(FilteringCatalogTests).Assembly);
+            var parent = new CompositionContainer(catalog);
+
+            var filteredCat = new FilteringCatalog(catalog, new HasCreationPolicy(CreationPolicy.NonShared));
+            var child = new CompositionContainer(filteredCat, parent);
+
+            var root = child.GetExportedValue<Root>();
+            var dep1 = root.Dep;
+            var dep2 = dep1.Dep;
+            var dep3 = dep2.Dep;
+
+            Assert.That(root.Disposed, Is.False);
+            Assert.That(dep1.Disposed, Is.False);
+            Assert.That(dep2.Disposed, Is.False);
+            Assert.That(dep3.Disposed, Is.False);
+            
+            child.Dispose();
+
+            Assert.That(root.Disposed, Is.True); // Disposed as it was created by the child container
+            Assert.That(dep1.Disposed, Is.True); // Disposed as it was created by the child container
+            Assert.That(dep2.Disposed, Is.False);
+            Assert.That(dep3.Disposed, Is.False);
+        }
+    }
+}
---
+++ src/MefContrib/Hosting/FilteringCatalog.cs
@@ -0,0 +1,85 @@
+namespace MefContrib.Hosting
+{
+    using System;
+    using System.ComponentModel.Composition.Hosting;
+    using System.ComponentModel.Composition.Primitives;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// Represents a catalog which wraps any <see cref="ComposablePartCatalog"/> and
+    /// filters out <see cref="ComposablePartDefinition"/>s based on a given criteria.
+    /// </summary>
+    public class FilteringCatalog : ComposablePartCatalog, INotifyComposablePartCatalogChanged
+    {
+        private readonly INotifyComposablePartCatalogChanged innerNotifyChange;
+        private readonly IQueryable<ComposablePartDefinition> partsQuery;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilteringCatalog"/> class.
+        /// </summary>
+        /// <param name="inner">A <see cref="ComposablePartCatalog"/> whose parts
+        /// are to be filtered based on a given criteria.</param>
+        /// <param name="expression">An <see cref="Expression"/> which defines the filter query.</param>
+        public FilteringCatalog(ComposablePartCatalog inner,
+                               Expression<Func<ComposablePartDefinition, bool>> expression)
+        {
+            if (inner == null)
+                throw new ArgumentNullException("inner");
+
+            if (expression == null)
+                throw new ArgumentNullException("expression");
+
+            this.innerNotifyChange = inner as INotifyComposablePartCatalogChanged;
+            this.partsQuery = inner.Parts.Where(expression);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FilteringCatalog"/> class.
+        /// </summary>
+        /// <param name="inner">A <see cref="ComposablePartCatalog"/> whose parts
+        /// are to be filtered based on a given criteria.</param>
+        /// <param name="filter">An instance of the <see cref="IFilter"/> interface
+        /// to be used as a filter query.</param>
+        public FilteringCatalog(ComposablePartCatalog inner, IFilter filter)
+            : this(inner, p => filter.Filter(p))
+        {
+        }
+
+        public override IQueryable<ComposablePartDefinition> Parts
+        {
+            get
+            {
+                return this.partsQuery;
+            }
+        }
+
+        public event EventHandler<ComposablePartCatalogChangeEventArgs> Changed
+        {
+            add
+            {
+                if (this.innerNotifyChange != null)
+                    this.innerNotifyChange.Changed += value;
+            }
+            remove
+            {
+                if (this.innerNotifyChange != null)
+                    this.innerNotifyChange.Changed -= value;
+            }
+        }
+
+        public event EventHandler<ComposablePartCatalogChangeEventArgs> Changing
+        {
+            add
+            {
+                if (this.innerNotifyChange != null)
+                    this.innerNotifyChange.Changing += value;
+            }
+            remove
+            {
+                if (this.innerNotifyChange != null)
+                    this.innerNotifyChange.Changing -= value;
+            }
+        }
+    }
+}
