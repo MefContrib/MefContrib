a989baa94c1bba6255be7434113da616fdc75ac6
Andreas Hakansson <andreas@selfinflicted.org>
2010-11-05 14:42:58 +0100

    Consolidated buildscripts into a single file for both versions

--- Build-Silverlight.bat
+++ Build-Silverlight.bat
@@ -1,3 +1,1 @@
-powershell -Command "& {Import-Module .\build\psake.psm1; $psake.use_exit_on_error = $true; Invoke-psake .\build\build_silverlight.ps1 -framework 4.0x86}"
-
-Pause
+powershell -Command "& {Import-Module .\build\psake.psm1; $psake.use_exit_on_error = $true; Invoke-psake .\build\build.ps1 -framework 4.0x86 -parameters @{solution='MefContrib.Silverlight'}}"
--- build/build.ps1
+++ build/build.ps1
@@ -11,7 +11,7 @@ include .\psake_ext.ps1
 task default -depends Test
 
 task Clean -description "This task cleans up the build directory" {
-    Remove-Item $build_directory\\MefContrib -Force -Recurse -ErrorAction SilentlyContinue
+    Remove-Item $build_directory\\$solution -Force -Recurse -ErrorAction SilentlyContinue
 }
 
 task Init -description "This tasks makes sure the build environment is correctly setup" {  
@@ -26,15 +26,15 @@ task Init -description "This tasks makes sure the build environment is correctly
 		-clsCompliant "false"
         
     if ((Test-Path $build_directory) -eq $false) {
-        New-Item $build_directory\\MefContrib -ItemType Directory
+        New-Item $build_directory\\$solution -ItemType Directory
     }
 }
 
 task Test -depends Compile -description "This task executes all tests" {
     $previous_directory = pwd
-    cd $build_directory\\MefContrib
+    cd $build_directory\\$solution
 
-    $testAssemblies = @(Get-ChildItem $build_directory\\MefContrib -Recurse -Include *.Tests.dll -Name | Split-Path -Leaf)
+    $testAssemblies = @(Get-ChildItem $build_directory\\$solution -Recurse -Include *.Tests.dll -Name | Split-Path -Leaf)
     
     foreach($assembly in $testAssemblies) {                   
         & $tools_directory\\nunit\\nunit-console.exe $assembly /nodots
@@ -48,8 +48,8 @@ task Test -depends Compile -description "This task executes all tests" {
 
 task Compile -depends Clean, Init -description "This task compiles the solution" {
     exec { 
-        msbuild $source_directory\MefContrib.sln `
-            /p:outdir=$build_directory\\MefContrib\\ `
+        msbuild $source_directory\$solution.sln `
+            /p:outdir=$build_directory\\$solution\\ `
             /verbosity:quiet `
             /p:Configuration=Release
     }
