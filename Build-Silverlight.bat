fe023bf6ca2452eb8a4ae9fbc6ed0ee69800543e
Andreas Hakansson <andreas@selfinflicted.org>
2010-11-13 13:58:05 +0100

    Updated buildscript for silverlight

--- Build-Silverlight.bat
+++ Build-Silverlight.bat
@@ -1,1 +1,3 @@
 powershell -Command "& {Import-Module .\build\psake.psm1; $psake.use_exit_on_error = $true; Invoke-psake .\build\build.ps1 -framework 4.0x86 -parameters @{solution='MefContrib.Silverlight'}}"
+
+Pause
