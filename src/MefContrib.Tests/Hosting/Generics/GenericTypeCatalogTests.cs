ad05f5a8134b48f455bee3a9adaa49f8fe2081e2
Piotr Wlodek <piotr.wlodek@gmail.com>
2010-12-14 11:08:07 +0100

    GenericTypeCatalog refactored and commented, more tests added

--- src/MefContrib.Tests/Hosting/Generics/GenericTypeCatalogTests.cs
+++ src/MefContrib.Tests/Hosting/Generics/GenericTypeCatalogTests.cs
@@ -10,11 +10,34 @@ namespace MefContrib.Hosting.Generics.Tests
     public class GenericTypeCatalogTests
     {
         [Test]
-        public void Passing_open_generic_type_causes_an_exception()
+        public void Passing_open_generic_type_as_the_exporting_member_causes_an_exception()
         {
             Assert.That(() =>
             {
                 new GenericTypeCatalog(typeof(Service1<>));
+            }, Throws.InstanceOf<ArgumentException>());
+        }
+
+        [Test]
+        public void Passing_closed_generic_type_as_the_contract_type_causes_an_exception()
+        {
+            Assert.That(() =>
+            {
+                new GenericTypeCatalog(typeof(Service1<Customer>), typeof(IService<Customer>));
+            }, Throws.InstanceOf<ArgumentException>());
+        }
+
+        [Test]
+        public void Passing_null_to_the_ctor_causes_an_exception()
+        {
+            Assert.That(() =>
+            {
+                new GenericTypeCatalog(null);
+            }, Throws.InstanceOf<ArgumentException>());
+
+            Assert.That(() =>
+            {
+                new GenericTypeCatalog(typeof(Service1<Customer>), null);
             }, Throws.InstanceOf<ArgumentException>());
         }
 
@@ -25,9 +48,9 @@ namespace MefContrib.Hosting.Generics.Tests
             var parts = typeCatalog.Parts.ToList();
             
             Assert.That(parts.Count, Is.EqualTo(1));
-            Assert.That(parts[0].ExportDefinitions.Single().ContractName,
+            Assert.That(parts[0].ExportDefinitions.First().ContractName,
                 Is.EqualTo(AttributedModelServices.GetContractName(typeof(Service1<Customer>))));
-            Assert.That(parts[0].ExportDefinitions.Single().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
+            Assert.That(parts[0].ExportDefinitions.First().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
                 Is.EqualTo(AttributedModelServices.GetTypeIdentity(typeof(Service1<Customer>))));
         }
 
@@ -38,9 +61,9 @@ namespace MefContrib.Hosting.Generics.Tests
             var parts = typeCatalog.Parts.ToList();
 
             Assert.That(parts.Count, Is.EqualTo(1));
-            Assert.That(parts[0].ExportDefinitions.Single().ContractName,
+            Assert.That(parts[0].ExportDefinitions.First().ContractName,
                 Is.EqualTo("contract-name"));
-            Assert.That(parts[0].ExportDefinitions.Single().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
+            Assert.That(parts[0].ExportDefinitions.First().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
                 Is.EqualTo(AttributedModelServices.GetTypeIdentity(typeof(Service2<Customer>))));
         }
 
@@ -51,9 +74,9 @@ namespace MefContrib.Hosting.Generics.Tests
             var parts = typeCatalog.Parts.ToList();
 
             Assert.That(parts.Count, Is.EqualTo(1));
-            Assert.That(parts[0].ExportDefinitions.Single().ContractName,
+            Assert.That(parts[0].ExportDefinitions.First().ContractName,
                 Is.EqualTo(AttributedModelServices.GetContractName(typeof(IService<Customer>))));
-            Assert.That(parts[0].ExportDefinitions.Single().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
+            Assert.That(parts[0].ExportDefinitions.First().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
                 Is.EqualTo(AttributedModelServices.GetTypeIdentity(typeof(IService<Customer>))));
         }
 
@@ -64,39 +87,142 @@ namespace MefContrib.Hosting.Generics.Tests
             var parts = typeCatalog.Parts.ToList();
 
             Assert.That(parts.Count, Is.EqualTo(1));
-            Assert.That(parts[0].ExportDefinitions.Single().ContractName,
+            Assert.That(parts[0].ExportDefinitions.First().ContractName,
                 Is.EqualTo("contract-name-2"));
-            Assert.That(parts[0].ExportDefinitions.Single().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
+            Assert.That(parts[0].ExportDefinitions.First().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
+                Is.EqualTo(AttributedModelServices.GetTypeIdentity(typeof(IService<Customer>))));
+        }
+
+        [Test]
+        public void When_querying_for_a_generic_part_exported_by_the_inherited_export_attribute_with_default_contract_name_and_type_the_closed_generic_part_is_returned()
+        {
+            var typeCatalog = new GenericTypeCatalog(typeof(InheritedService1Impl<Customer>), typeof(IInheritedService1<>));
+            var parts = typeCatalog.Parts.ToList();
+
+            Assert.That(parts.Count, Is.EqualTo(1));
+            Assert.That(parts[0].ExportDefinitions.Skip(2).First().ContractName,
+                Is.EqualTo(AttributedModelServices.GetContractName(typeof(IInheritedService1<Customer>))));
+            Assert.That(parts[0].ExportDefinitions.Skip(2).First().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
+                Is.EqualTo(AttributedModelServices.GetTypeIdentity(typeof(IInheritedService1<Customer>))));
+        }
+
+        [Test]
+        public void When_querying_for_a_generic_part_exported_by_the_inherited_export_attribute_with_a_given_contract_name_and_default_contract_type_the_closed_generic_part_is_returned()
+        {
+            var typeCatalog = new GenericTypeCatalog(typeof(InheritedService2Impl<Customer>), typeof(IInheritedService2<>));
+            var parts = typeCatalog.Parts.ToList();
+
+            Assert.That(parts.Count, Is.EqualTo(1));
+            Assert.That(parts[0].ExportDefinitions.Skip(2).First().ContractName,
+                Is.EqualTo("contract-name"));
+            Assert.That(parts[0].ExportDefinitions.Skip(2).First().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
+                Is.EqualTo(AttributedModelServices.GetTypeIdentity(typeof(IInheritedService2<Customer>))));
+        }
+
+        [Test]
+        public void When_querying_for_a_generic_part_exported_by_the_inherited_export_attribute_with_a_given_contract_name_and_type_the_closed_generic_part_is_returned()
+        {
+            var typeCatalog = new GenericTypeCatalog(typeof(InheritedService3Impl<Customer>), typeof(IService<>));
+            var parts = typeCatalog.Parts.ToList();
+
+            Assert.That(parts.Count, Is.EqualTo(1));
+            Assert.That(parts[0].ExportDefinitions.First().ContractName,
+                Is.EqualTo("contract-name-2"));
+            Assert.That(parts[0].ExportDefinitions.First().Metadata[CompositionConstants.ExportTypeIdentityMetadataName],
                 Is.EqualTo(AttributedModelServices.GetTypeIdentity(typeof(IService<Customer>))));
         }
     }
-
-    [Export(typeof(IS))]
-    public class S : IS {}
     
-    public interface IS {}
-
     [Export]
     public class Service1<T>
     {
+        [Export("Foo-Method")]
+        public void FooMethod() {}
+
+        [Export]
+        public string FooProperty
+        {
+            get { return "foo"; }
+        }
     }
 
     [Export("contract-name")]
     public class Service2<T>
     {
+        [Export("Foo-Method")]
+        public void FooMethod() { }
+
+        [Export]
+        public string FooProperty
+        {
+            get { return "foo"; }
+        }
     }
 
     [Export(typeof(IService<>))]
     public class ServiceImpl1<T> : IService<T>
     {
+        [Export("Foo-Method")]
+        public void FooMethod() { }
+
+        [Export]
+        public string FooProperty
+        {
+            get { return "foo"; }
+        }
     }
 
     [Export("contract-name-2", typeof(IService<>))]
     public class ServiceImpl2<T> : IService<T>
     {
+        [Export("Foo-Method")]
+        public void FooMethod() { }
+
+        [Export]
+        public string FooProperty
+        {
+            get { return "foo"; }
+        }
     }
 
     public interface IService<T> { }
+
+    [InheritedExport]
+    public interface IInheritedService1<T> { }
+
+    public class InheritedService1Impl<T> : IInheritedService1<T>
+    {
+        [Export("Foo-Method")]
+        public void FooMethod() { }
+
+        [Export]
+        public string FooProperty
+        {
+            get { return "foo"; }
+        }
+    }
+
+    [InheritedExport("contract-name")]
+    public interface IInheritedService2<T> { }
+
+    public class InheritedService2Impl<T> : IInheritedService2<T>
+    {
+        [Export("Foo-Method")]
+        public void FooMethod() { }
+
+        [Export]
+        public string FooProperty
+        {
+            get { return "foo"; }
+        }
+    }
+
+    [InheritedExport("contract-name-2", typeof(IService<>))]
+    public interface IInheritedService3<T> { }
+
+    public class InheritedService3Impl<T> : IInheritedService3<T>, IService<T>
+    {
+    }
 
     public class Customer { }
 }
--- src/MefContrib/Hosting/Generics/GenericTypeCatalog.cs
+++ src/MefContrib/Hosting/Generics/GenericTypeCatalog.cs
@@ -8,36 +8,70 @@ namespace MefContrib.Hosting.Generics
     using System.ComponentModel.Composition.ReflectionModel;
     using System.Linq;
 
+    /// <summary>
+    /// Defines a catalog which produces closed-generic parts.
+    /// </summary>
+    /// <remarks>
+    /// The catalog can rewrite <see cref="ComposablePartDefinition"/> and its <see cref="ExportDefinition"/>
+    /// instances so that the type identity and optionally the contract name is always closed-generic.
+    /// </remarks>
     public class GenericTypeCatalog : ComposablePartCatalog
     {
         private readonly object synchRoot = new object();
         private IQueryable<ComposablePartDefinition> innerPartsQueryable;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericTypeCatalog"/> class.
+        /// </summary>
+        /// <param name="exportingType">The exporting type. This has to be closed-generic type.</param>
         public GenericTypeCatalog(Type exportingType)
-            : this(exportingType, exportingType.GetGenericTypeDefinition())
         {
+            ValidateExportingType(exportingType);
+
+            var contractType = exportingType.GetGenericTypeDefinition();
+            ValidateContractType(contractType);
+
+            ExportingType = exportingType;
+            ContractType = contractType;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericTypeCatalog"/> class.
+        /// </summary>
+        /// <param name="exportingType">The exporting type. This has to be closed-generic type.</param>
+        /// <param name="contractType">The contract type. This has to be open-generic type.</param>
         public GenericTypeCatalog(Type exportingType, Type contractType)
+        {
+            ValidateExportingType(exportingType);
+            ValidateContractType(contractType);
+
+            ExportingType = exportingType;
+            ContractType = contractType;
+        }
+
+        private static void ValidateExportingType(Type exportingType)
         {
             if (exportingType == null)
             {
                 throw new ArgumentNullException("exportingType");
             }
 
-            if (contractType == null)
-            {
-                throw new ArgumentNullException("contractType");
-            }
-
             if (!exportingType.IsGenericType)
             {
                 throw new ArgumentException("Parameter exportingType is not a generic type.");
             }
-            
+
             if (exportingType.IsGenericTypeDefinition)
             {
                 throw new ArgumentException("Parameter exportingType cannot be an open generic type.");
+            }
+        }
+
+        private static void ValidateContractType(Type contractType)
+        {
+            if (contractType == null)
+            {
+                throw new ArgumentNullException("contractType");
             }
 
             if (!contractType.IsGenericType)
@@ -49,13 +83,16 @@ namespace MefContrib.Hosting.Generics
             {
                 throw new ArgumentException("Parameter contractType is not an open generic type.");
             }
-
-            ExportingType = exportingType;
-            ContractType = contractType;
         }
 
+        /// <summary>
+        /// Gets the part type which declares the exports. This is always a closed-generic type.
+        /// </summary>
         public Type ExportingType { get; private set; }
 
+        /// <summary>
+        /// Gets the contract type. This is always open-generic type.
+        /// </summary>
         public Type ContractType { get; private set; }
         
         public override IQueryable<ComposablePartDefinition> Parts
@@ -74,7 +111,6 @@ namespace MefContrib.Hosting.Generics
                         var catalog = new TypeCatalog(ExportingType);
                         this.innerPartsQueryable = catalog.Parts
                             .Select(RewritePart)
-                            .Where(t => t != null)
                             .ToList()
                             .AsQueryable();
                     }
@@ -86,49 +122,63 @@ namespace MefContrib.Hosting.Generics
 
         private ComposablePartDefinition RewritePart(ComposablePartDefinition definition)
         {
-            var exportDefinition = definition.ExportDefinitions.FirstOrDefault(t => CompositionServices.GetExportDefinitionType(t) == ExportingType);
-            if (exportDefinition != null)
+            var closedGenericContractType = ContractType.MakeGenericType(ExportingType.GetGenericArguments());
+            var closedGenericTypeIdentity = AttributedModelServices.GetTypeIdentity(closedGenericContractType);
+            var openGenericTypeIdentity = AttributedModelServices.GetTypeIdentity(ContractType);
+            var openGenericContractName = AttributedModelServices.GetContractName(ContractType);
+            var exports = new List<ExportDefinition>();
+            var hasRewrittenExports = false;
+
+            foreach (var exportDefinition in definition.ExportDefinitions)
             {
-                var closedGenericContractType = ContractType.MakeGenericType(ExportingType.GetGenericArguments());
-                var typeIdentity = AttributedModelServices.GetTypeIdentity(closedGenericContractType);
-                if (typeIdentity == (string) exportDefinition.Metadata[CompositionConstants.ExportTypeIdentityMetadataName])
+                // Rewrite only exports having open-generics type identity of the contract type we do care about
+                if (openGenericTypeIdentity == (string)exportDefinition.Metadata[CompositionConstants.ExportTypeIdentityMetadataName])
                 {
-                    return definition;
+                    // If both open-generic contract name and the present contract name are equal,
+                    // contract name has to be rewritten to form a closed-generic contract
+                    var contractName = openGenericContractName == exportDefinition.ContractName
+                                           ? AttributedModelServices.GetContractName(closedGenericContractType)
+                                           : exportDefinition.ContractName;
+
+                    // Preserve all the metadata except the type identity as it has to be rewritten
+                    var metadata = new Dictionary<string, object>
+                    {
+                        { CompositionConstants.ExportTypeIdentityMetadataName, closedGenericTypeIdentity }
+                    };
+                    foreach (var key in exportDefinition.Metadata.Keys.Where(key => key != CompositionConstants.ExportTypeIdentityMetadataName))
+                    {
+                        metadata.Add(key, exportDefinition.Metadata[key]);
+                    }
+                    
+                    // Rewrite the export
+                    var rewrittenExport = ReflectionModelServices.CreateExportDefinition(
+                        ReflectionModelServices.GetExportingMember(exportDefinition),
+                        contractName,
+                        new Lazy<IDictionary<string, object>>(() => metadata),
+                        exportDefinition as ICompositionElement);
+                    
+                    exports.Add(rewrittenExport);
+                    hasRewrittenExports = true;
                 }
-
-                var contractName = AttributedModelServices.GetContractName(ContractType);
-
-                // If both are equal, contract name has to be rewritten
-                contractName = contractName == exportDefinition.ContractName ? AttributedModelServices.GetContractName(closedGenericContractType) : exportDefinition.ContractName;
-
-                var metadata = new Dictionary<string,object>();
-                foreach (var key in exportDefinition.Metadata.Keys.Where(key => key != CompositionConstants.ExportTypeIdentityMetadataName))
+                else
                 {
-                    metadata.Add(key, exportDefinition.Metadata[key]);
+                    // This export is ok, copy the original
+                    exports.Add(exportDefinition);
                 }
-                metadata.Add(CompositionConstants.ExportTypeIdentityMetadataName, typeIdentity);
+            }
 
-                var rewrittenExport = ReflectionModelServices.CreateExportDefinition(
-                    new LazyMemberInfo(ExportingType),
-                    contractName,
-                    new Lazy<IDictionary<string, object>>(() => metadata),
-                    null);
-                
-                var exports = new List<ExportDefinition>();
-                exports.Add(rewrittenExport);
-                exports.AddRange(definition.ExportDefinitions.Skip(1));
-
-                var part = ReflectionModelServices.CreatePartDefinition(
-                    new Lazy<Type>(() => ExportingType),
+            // If the part has any rewritten exports, we have to rewrite the part itself
+            if (hasRewrittenExports)
+            {
+                return ReflectionModelServices.CreatePartDefinition(
+                    ReflectionModelServices.GetPartType(definition),
                     ReflectionModelServices.IsDisposalRequired(definition),
                     new Lazy<IEnumerable<ImportDefinition>>(() => definition.ImportDefinitions),
                     new Lazy<IEnumerable<ExportDefinition>>(() => exports),
                     new Lazy<IDictionary<string, object>>(() => definition.Metadata),
-                    null);
-
-                return part;
+                    definition as ICompositionElement);
             }
-            
+
             return definition;
         }
     }
