456d2431fda7f43acd1488ca90b6ea0b1cf535ec
maartenba <maarten@maartenballiauw.be>
2011-01-26 11:07:52 +0100

    Changed NuGet packages

--- Build-NuGet.bat
+++ Build-NuGet.bat
@@ -1,9 +1,12 @@
 call build.bat
 
 ; MefContrib
-copy .\release\MefContrib\MefContrib.* .\nuget\MefContrib\lib
+copy .\release\MefContrib\MefContrib.dll .\nuget\MefContrib\lib
+copy "%PROGRAMFILES%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0\Profile\Client\System.ComponentModel.Composition.dll" .\nuget\MefContrib\lib
+copy "%PROGRAMFILES(X86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0\Profile\Client\System.ComponentModel.Composition.dll" .\nuget\MefContrib\lib
 .\tools\nuget\NuGet.exe pack .\nuget\MefContrib\MefContrib.nuspec
 del /Q .\nuget\MefContrib\lib\*.*
+
 
 ; MefContrib.MVC3
 copy .\release\MefContrib\MefContrib.Web.* .\nuget\MefContrib.MVC3\lib
@@ -12,6 +15,6 @@ copy "%PROGRAMFILES(X86)%\Reference Assemblies\Microsoft\Framework\.NETFramework
 .\tools\nuget\NuGet.exe pack .\nuget\MefContrib.MVC3\MefContrib.MVC3.nuspec
 del /Q .\nuget\MefContrib.MVC3\lib\*.*
 
-copy .\*.nupkg .\release
+move .\*.nupkg .\release
 
 Pause
