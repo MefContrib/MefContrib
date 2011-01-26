23c76defd5ca178881bc63c605fb63cfc8f21674
maartenba <maarten@balliauw.be>
2011-01-26 20:57:17 +0100

    Added extra NuGet packages for unity and castle

--- Build-NuGet.bat
+++ Build-NuGet.bat
@@ -8,6 +8,18 @@ copy "%PROGRAMFILES(X86)%\Reference Assemblies\Microsoft\Framework\.NETFramework
 del /Q .\nuget\MefContrib\lib\*.*
 
 
+; MefContrib.Interception.Castle
+copy .\release\MefContrib\MefContrib.Hosting.Interception.Castle.dll .\nuget\MefContrib.Interception.Castle\lib
+.\tools\nuget\NuGet.exe pack .\nuget\MefContrib.Interception.Castle\MefContrib.Interception.Castle.nuspec
+del /Q .\nuget\MefContrib.Interception.Castle\lib\*.*
+
+
+; MefContrib.Integration.Unity
+copy .\release\MefContrib\MefContrib.Integration.Unity.dll .\nuget\MefContrib.Integration.Unity\lib
+.\tools\nuget\NuGet.exe pack .\nuget\MefContrib.Integration.Unity\MefContrib.Integration.Unity.nuspec
+del /Q .\nuget\MefContrib.Integration.Unity\lib\*.*
+
+
 ; MefContrib.MVC3
 copy .\release\MefContrib\MefContrib.Web.* .\nuget\MefContrib.MVC3\lib
 copy "%PROGRAMFILES%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0\Profile\Client\System.ComponentModel.Composition.dll" .\nuget\MefContrib.MVC3\lib
